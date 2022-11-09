92e1a751fe8e712d1d6864703170beeebe712df7
Adhoc123 <emonhasan1010@gmail.com>
2022-11-09 20:42:06 +0600

    modified route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -16,7 +16,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
       const dataCollection = client.db("betterLife").collection("services");
-      
+      app.get('/services', async(req,res)=>{
+        const query = {};
+        const cursor = dataCollection.find(query);
+        const services = await cursor.limit(3).toArray();
+        res.send(services);
+      })
+      app.get('/totalservices', async(req,res)=>{
+        const query = {};
+        const cursor = dataCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+      })
+      app.get('/services/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = { _id: ObjectId(id) };
+        const service = await dataCollection.findOne(query);
+        res.send(service);
+      })
     }
     finally {
     
